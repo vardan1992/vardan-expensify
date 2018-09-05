afef39ec9f5073975345df04419a7983e336c175
Vardan Gupta <vardan1992@gmail.com>
2018-09-06 01:53:22 +0530

    removed dummy data

--- src/app.js
+++ src/app.js
@@ -19,29 +19,6 @@ import './styles/style.scss';
 const store = configureStore();
 
 
-store.dispatch(addExpense({
-    description: 'water bill',
-    amount: 200,
-    createdAt: 15422
-}))
-
-store.dispatch(addExpense({
-    description: 'gas bill',
-    amount: 400,
-    createdAt: -4554545
-}))
-
-store.dispatch(addExpense({
-    description: 'electricity bill',
-    amount: 1000,
-    createdAt: 4554445457884
-}))
-//store.dispatch(setTextFilter('bill'));
-
-
-const state = store.getState();
-
-console.log(getVisibleExpenses(state.expenses, state.filters));
 
 const jsx = (
     <Provider store ={store}>
